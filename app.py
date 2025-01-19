ef6ccda5fd19273a5de761716832682dbcd9a75d
amirghadiri1987 <144431373+amirghadiri1987@users.noreply.github.com>
2025-01-19 12:10:35 +0330

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__)
 
 # Set the directory to save files
-UPLOAD_FOLDER = '/ServerUpload/1001/'
+UPLOAD_FOLDER = '/root/EA_Server/ServerUpload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/upload_csv', methods=['POST'])
