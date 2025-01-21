7d070b1d802855602aaa70a687a2ad07acd2b9b4
amirghadiri1987 <144431373+amirghadiri1987@users.noreply.github.com>
2025-01-21 20:34:34 +0330

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 UPLOAD_FOLDER = '/root/EA_Server/ServerUpload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
 @app.route('/upload_csv', methods=['POST'])
 def upload_csv():
     client_id = request.form.get('clientID')
@@ -16,7 +15,7 @@ def upload_csv():
 
     if 'file' not in request.files:
         return jsonify({'status': 'fail', 'message': 'No file part'}), 400
-    
+
     file = request.files['file']
     if file.filename == '':
         return jsonify({'status': 'fail', 'message': 'No selected file'}), 400
@@ -25,21 +24,24 @@ def upload_csv():
     client_folder = os.path.join(app.config['UPLOAD_FOLDER'], client_id)
     os.makedirs(client_folder, exist_ok=True)
 
-    # Save the file in the client's folder
+    # Check if the file already exists
     file_path = os.path.join(client_folder, file.filename)
+    if os.path.exists(file_path):
+        return jsonify({'status': 'fail', 'message': 'File already exists', 'path': file_path}), 409
+
+    # Save the file in the client's folder
     file.save(file_path)
 
     return jsonify({'status': 'success', 'message': 'File uploaded successfully', 'path': file_path}), 200
 
 @app.route('/check_file', methods=['GET'])
 def check_file():
-    file_path = "/root/EA_Server/ServerUpload/Trade_Transaction.csv"
+    file_path = "/root/EA_Server/ServerUpload/1001/Trade_Transaction.csv"
     
     if os.path.exists(file_path) and os.path.isfile(file_path):
         return jsonify({'status': 'success', 'message': 'File found', 'path': file_path}), 200
     else:
         return jsonify({'status': 'fail', 'message': 'File not found'}), 404
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
